c70bd1c35ae2ac6918f02a68f77d2507509e295d
Israel Ortuño <ai.ortuno@gmail.com>
2015-12-02 19:13:15 +0100

    Added Laravel support

--- src/PipedriveServiceProvider.php
+++ src/PipedriveServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Devio\Pipedrive;
 
+use Devio\Pipedrive\Exceptions\PipedriveException;
 use Illuminate\Support\ServiceProvider;
 
 class PipedriveServiceProvider extends ServiceProvider
@@ -13,5 +14,22 @@ class PipedriveServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton('pipedrive', function ($app) {
+            $token = $app['config']->get('services.pipedrive.token');
+
+            if (! $token) {
+                throw new PipedriveException('Pipedrive was not configured in services.php configuration file.');
+            }
+
+            return new Pipedrive($token);
+        });
+    }
+
+    /**
+     * @return array
+     */
+    public function provides()
+    {
+        return ['pipedrive'];
     }
 }
