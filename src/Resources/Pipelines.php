5d0e92475ecd291625991684b7e1c3afe13eeb4f
Israel Ortuño <ai.ortuno@gmail.com>
2015-12-02 19:14:17 +0100

    Pipeline can list deals

--- src/Resources/Pipelines.php
+++ src/Resources/Pipelines.php
@@ -3,9 +3,12 @@
 namespace Devio\Pipedrive\Resources;
 
 use Devio\Pipedrive\Resources\Basics\Resource;
+use Devio\Pipedrive\Resources\Traits\ListsDeals;
 
 class Pipelines extends Resource
 {
+    use ListsDeals;
+
     /**
      * Disabled abstract methods.
      *
